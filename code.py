dba19060ecf5b98dee866f30a8e16825dfe68696
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-04-19 18:41:45 +0100

    Possibly sorted out the display code

--- code.py
+++ code.py
@@ -3,6 +3,8 @@ import board
 import digitalio
 from adafruit_onewire.bus import OneWireBus
 from adafruit_ds18x20 import DS18X20
+import busio
+import adafruit_ssd1306
 
 # ProgIndicator Setup
 progIndicator = digitalio.DigitalInOut(board.LED)
@@ -12,5 +14,12 @@ progIndicator.direction = digitalio.Direction.OUTPUT
 ow_bus = OneWireBus(board.D2)
 temperature = DS18X20(ow_bus, ow_bus.scan()[0])
 
+# Display Setup
+i2c = busio.I2C(sda=board.GP0, scl=board.GP1)
+display = adafruit_ssd1306.SSD1306_I2C(27, 27, i2c)
+def displayTemperatureEdit(temp):
+	display.fill(0)
+	display.text("Temperature: " + str(float(temp)) + "°C")
+	display.show()
 while True:
 	progIndicator.value = True
