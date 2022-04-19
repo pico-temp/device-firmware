18eaa3f4a27414f7cd6e22daed534a2bdbfb33ed
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-04-19 17:21:34 +0100

    added temperature setup code

--- code.py
+++ code.py
@@ -1,10 +1,16 @@
 # Imports
 import board
 import digitalio
+from adafruit_onewire.bus import OneWireBus
+from adafruit_ds18x20 import DS18X20
 
 # ProgIndicator Setup
 progIndicator = digitalio.DigitalInOut(board.LED)
 progIndicator.direction = digitalio.Direction.OUTPUT
+
+# Temperature Setup
+ow_bus = OneWireBus(board.D2)
+temperature = DS18X20(ow_bus, ow_bus.scan()[0])
 
 while True:
 	progIndicator.value = True
